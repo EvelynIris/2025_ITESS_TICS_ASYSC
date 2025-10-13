9434d26fefb710ae323b6e087adbd35fe286e169
Evelyn Iris Hernandez Vigil <evelynirisvigil@gmail.com>
2025-10-13 14:35:29 -0600

    Primer repositorio

---
+++ Unidad2_Seriers_de_Fourier/hola.py
@@ -0,0 +1,22 @@
+#Materia: Analisis de senales y sistemas de comunicacion
+#Maestro: Dr. Francisco Javier Montecillo Puente
+#Periodo: SEPTIEMBRE-DICIEMBRE 2025
+#Unidad 3
+#Alumna: Evelyn Iris Hernandez Vigil
+#suma, multiplicacion, division con doble diagonal, resta, modulo, exponente
+print ("Hola mundo")
+
+a = 5
+b = 2
+
+sumResul = a + b
+print("Suma: " + sumResul)
+
+resResul = a - b
+print("Resta: " + resResul)
+
+divResul = a / b
+print("Divison: " + divResul)
+
+diviResul1 = a // b
+print("Division 2: " + diviResul)
